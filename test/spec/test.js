9cb616220791fafb7d36a7662ce426433b4f1136
nfrasser <nfrasser@gmail.com>
2014-01-25 00:00:26 -0500

    Intro Unit tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,8 +1,32 @@
 /* global test, ok, module */
 
-module('Integration Tests');
+module('Integration Tests - Main Test');
 
 test('Components and namespace exist', function () {
     ok(true, 'Should run here few assertions');
     ok(TC, 'TC namespace should exist');
+});
+
+module('Button Bar', {
+	setup: function () {
+		App.reset();
+		App.visit('/');
+	}
+});
+
+test('Basic button bar', function () {
+	var buttonBar = App.TopcoatButtonBar.create({
+		content: [
+			'one',
+			'two',
+			'three'
+		]
+	});
+
+	buttonBar.appendView();
+
+	andThen(function () {
+		ok(find('#' + button.elementId));
+
+	});
 });
